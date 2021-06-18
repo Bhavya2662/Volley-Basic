bda42a6a0c33804f89e8d438246d3a3b43a6344f
Bhavya Sharma <bhavyaas2662@hotmail.com>
2021-06-18 09:41:08 +0530

    first commit

---
+++ app/src/main/java/com/example/volleybasic/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.volleybasic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    RequestQueue requestQueue;
+    StringRequest stringRequest;
+    String url = "https://run.mocky.io/v3/ee18d065-d9b5-41b7-8591-32191901676b";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button =findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendAndRequestResponse();
+            }
+        });
+    }
+    private void sendAndRequestResponse(){
+        requestQueue = Volley.newRequestQueue(this);
+        stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_LONG).show();
+            }
+        });
+        requestQueue.add(stringRequest);
+    }
+}
